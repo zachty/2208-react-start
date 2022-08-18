200ceeca0ed526a1cd7ef89ed1eb99e038e5b662
Zach Byerley <zachbyerley@gmail.com>
2022-08-18 13:18:49 -0400

    finished lab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import ListItem from './compenents/ListItem';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const list = ['toggle', 'click me', 'change'];
+
+    return (
+        <div className="App">
+            {list.map((item, index) => (
+                <ListItem name={item} key={index} />
+            ))}
+        </div>
+    );
 }
 
 export default App;
